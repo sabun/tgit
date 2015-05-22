1932b5e643ef17f32014fd9576666b269fd53f0d
santoabun <santo@livaza.com>
2015-05-22 23:11:41 +0700

    lvl 3-5

--- index.php
+++ index.php
@@ -7,10 +7,16 @@
 
 	echo "sabun 4";
 
-	echo "lvz 1"
+	echo "lvz 1";
 	
-	echo "sabun 5 nempel 6";
-	echo "sabun 6 nempel 5";
+	echo "sabun 5";
 	
-	echo "lvz 2"
+	echo "sabun 6";
+	
+	echo "lvz 2";
+	
+	echo "lvz 3";
+	
+	echo "lvz 4 nempel 5";
+	echo "lvz 5 nempel 4";
 ?>
